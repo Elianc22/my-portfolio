ae909c8d4b79922766defc8870ff719e518b07df
Elianc22 <chavezelian2000@gmail.com>
2023-05-07 06:46:15 +0200

    Styling scrollbar and creating about me section

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -3,12 +3,43 @@ import "./_aboutStyle.scss";
 const About = () => {
   return (
     <div className="about-container">
-      <div>
-        <p className="about-title">About me</p>
+      <p className="about-title">About me 👨‍💻</p>
+      <div className="about-content">
+        <div className="about-img-container">
+          <div>
+            <img src="/portfolio.png" />
+          </div>
+        </div>
         <div className="about-my-info">
           <p>
-            I am Elian Chavez Ferreyra and this is my portfolio presentation,
-            lets get to know each other!
+            ¡Hola! Mi nombre es Elian Chavez, tengo 22 años y nací en Argentina.
+            Hace aproximadamente un año decidí mudarme a Málaga, España en busca
+            de nuevas oportunidades y experiencias. Fue aquí donde decidí que
+            deseaba dejar de ver la programación como un hobby y tomarlo de
+            forma profesional. En 2021 comencé a estudiar programación por
+            curiosidad, pero rápidamente me di cuenta de que esto era algo que
+            realmente disfrutaba y quería seguir aprendiendo.{" "}
+          </p>
+          <p>
+            {" "}
+            Aparte de mi amor por la programación, soy un gran fanático de los
+            deportes. Siempre he sido muy activo y disfruto de todo tipo de
+            deportes, especialmente el fútbol y el baloncesto. Me gusta
+            mantenerme en forma y siempre trabajo en mejorar mi salud y estado
+            físico.
+          </p>
+          <p>
+            En mi tiempo libre, uno de mis pasatiempos favoritos es armar cubos
+            Rubik. Es algo que me encanta hacer porque me ayuda a relajarme y
+            concentrarme. De hecho, mi record personal para resolver un cubo
+            Rubik es de 1 minuto.
+          </p>
+          <p>
+            En resumen, soy una persona apasionada por la programación, amante
+            de los deportes y siempre trabajando en mejorarme a mí mismo.
+            Siempre estoy abierto a nuevas oportunidades y desafíos, y espero
+            poder seguir aprendiendo y creciendo en el mundo de la programación
+            y más allá.
           </p>
         </div>
       </div>
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,11 +1,23 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./_projectsStyle.scss";
+import { useEffect } from "react";
 
 const Projects = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      delay: 100,
+      offset: 200,
+      easing: "ease-in-quad",
+    });
+  }, []);
+
   return (
     <section className="project">
       <p className="project-title">My Projects 💻</p>
       <aside className="project-cards-container">
-        <div>
+        <div data-aos="flip-right">
           <img src="/rick-and-morty-project.png" />
           <article>
             <p className="project-tags">JavaScript</p>
@@ -15,7 +27,7 @@ const Projects = () => {
             <p className="project-tags">ReactJs</p>
           </article>
         </div>
-        <div>
+        <div data-aos="flip-right">
           <img src="/tres-en-raya-project.png" />
           <article>
             <p className="project-tags">JavaScript</p>
@@ -24,7 +36,7 @@ const Projects = () => {
             <p className="project-tags">ReactJs</p>
           </article>
         </div>
-        <div>
+        <div data-aos="flip-right">
           <img src="/shopping-cart-project.png" />
           <article>
             <p className="project-tags">JavaScript</p>
